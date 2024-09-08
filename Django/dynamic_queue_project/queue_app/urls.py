8e7640ca3597699c01001239842767272c484917
Veda-rathna <gamingguru245@gmail.com>
2024-09-08 20:46:53 +0530

    removed unused url

--- Django/dynamic_queue_project/queue_app/urls.py
+++ Django/dynamic_queue_project/queue_app/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('arrive/', views.patient_arrival, name='patient_arrival'),
-    path('status/', views.queue_status, name='queue_status'),
     path('queue/status/', views.queue_status, name='queue_status'),
     path('queue/clear/', views.clear_queue, name='clear_queue'),
-    path('login/', views.login_view, name='login_view'),
     path('queue_status/', views.queue_status, name='queue_status'),
     path('patient_arrival/', views.patient_arrival, name='patient_arrival'),
 ]
